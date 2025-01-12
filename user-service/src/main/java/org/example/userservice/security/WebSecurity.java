fcfca8767c389cd7103b099e667b3374f81598e8
DongHoonYu96 <sbna154@gmail.com>
2025-01-12 19:58:11 +0900

    feat: 인증 필터 구현

--- user-service/src/main/java/org/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/org/example/userservice/security/WebSecurity.java
@@ -1,26 +1,59 @@
 package org.example.userservice.security;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.example.userservice.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
 
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()  // 모든 요청 허용
                 )
-                .httpBasic(httpBasic -> httpBasic.disable());
+                .addFilter(getAuthenticationFilter(authenticationManager));  // AuthenticationManager 전달
 
         return http.build();
+    }
+
+    private Filter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/login"); // URL 지정
+        return authenticationFilter;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authProvider;
     }
 
 //    @Bean
