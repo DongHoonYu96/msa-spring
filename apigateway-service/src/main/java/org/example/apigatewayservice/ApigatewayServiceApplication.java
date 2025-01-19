aa2aa8a932ffd8a0c962279273115aea0d9ad79f
DongHoonYu96 <sbna154@gmail.com>
2025-01-20 00:54:34 +0900

    feat: config profile 설정
    ecommerce-dev 를 참조

--- apigateway-service/src/main/java/org/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/org/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,27 @@
 package org.example.apigatewayservice;
 
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
+    }
+
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
